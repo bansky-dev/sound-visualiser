2d40bc44da01f5acafebdf82c2d1c1d7dab115dc
Kociarz <dbednorz@tuta.io>
2024-10-03 11:57:39 +0200

    first commit

---
+++ music-visualiser/Program.cs
@@ -0,0 +1,13 @@
+﻿using NAudio;
+using NAudio.WaveFormRenderer; 
+
+namespace music_visualiser;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // SOUND VISUALISER MADE BY BANSKY (NOT THE BRI'ISH ARTIST)
+
+    }
+}
