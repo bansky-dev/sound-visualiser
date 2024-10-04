29c12ad55bbf4d1799e5c1436db64d1daefeb903
Kociarz <dbednorz@tuta.io>
2024-10-04 11:09:57 +0200

    Made menu improvemts, a little refractoring is needed.

--- music-visualiser/Program.cs
+++ music-visualiser/Program.cs
@@ -1,10 +1,13 @@
-﻿using NAudio;
+﻿using System.Net;
+using NAudio;
 using NAudio.WaveFormRenderer; 
 
 namespace music_visualiser;
 
 class Program
 {
+    // var renderingConfig = new StandardWaveFormRendererSettings();
+
     static void Main(string[] args)
     {
         // SOUND VISUALISER MADE BY BANSKY (NOT THE BRI'ISH ARTIST)
@@ -13,18 +16,31 @@ class Program
         System.Console.Write("Choose your file path (mp3 file)");
         string mp3FilePathChoice = Console.ReadLine();
         System.Console.WriteLine("Now, choose your config");
+        System.Console.Write("Choose the width for your image in px (640px recomended): ");
+        int userWidthValue  = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Choose the top height: ");
+        int userTopHeightValue = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Choose the bottom height: ");
+        int userBottomHeightValue = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Choose the output file name (without the file extension at the end): ");
+        string userOutputFileName = Console.ReadLine();
 
-        //TODO: finish the menu
+        //TODO: refractor and fix
 
         var renderingSettings = new StandardWaveFormRendererSettings();
-        renderingSettings.Width = 640;
-        renderingSettings.TopHeight = 32;
-        renderingSettings.BottomHeight = 32;
+        var audioPeakProvider = new MaxPeakProvider();
+        renderingSettings.Width = userWidthValue;
+        renderingSettings.TopHeight = userTopHeightValue;
+        renderingSettings.BottomHeight = userBottomHeightValue;
 
         var renderer = new WaveFormRenderer();
-        var audioFilePath = "sound.mp3";
-        var image = renderer.Render(audioFilePath, myPeakProvider, myRendererSettings);
+        string audioFilePath = "sound.mp3";
+        var image = renderer.Render(audioFilePath, audioPeakProvider, renderingSettings);
 
-        image.Save("filename.png", ImageFormat.png)
+        image.Save(userOutputFileName);
+    }
+
+    public void visualiseSound(){
+
     }
 }
--- music-visualiser/obj/Debug/net8.0/music-visualiser.AssemblyInfo.cs
+++ music-visualiser/obj/Debug/net8.0/music-visualiser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("music-visualiser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbb1a4e7c1511b82cdd3e23f273f60e281399b8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("music-visualiser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("music-visualiser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
